ed1fcb01d2d849b5060e09c62e863eef6e001188
Elisey Esaulkov <toelisey@yandex.ru>
2023-06-08 21:59:06 +0500

    feat: add AboutMe, Portfolio; adjust Main page

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './Portfolio.css';
+import {
+  PORTFOLIO__TITLE,
+  PORTFOLIO_LINKS_LIST,
+  PORTFOLIO__ARROW,
+} from './constants'
+import { Link } from 'react-router-dom'
 
 function Portfolio() {
 
   return(
-    <div className='portfolio'>
-
-    </div>
+    <section className='portfolio'>
+      <h2 className='portfolio__title'>{PORTFOLIO__TITLE}</h2>
+      <nav className='portfolio__nav'>
+      {PORTFOLIO_LINKS_LIST.map((item, index) => (
+        <li
+        className='portfolio__nav-item'
+        key={index}>
+          <Link to={item.link} className='portfolio__link'>{item.name}</Link>
+          <p className='portfolio__link-symbol'>{PORTFOLIO__ARROW}</p>
+        </li>
+        )
+      )}
+      </nav>
+    </section>
   );
 }
 export default Portfolio;
