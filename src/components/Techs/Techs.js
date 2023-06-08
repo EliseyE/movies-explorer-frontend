7f2a60e83485951006cbdd311d738d6ee413c171
Elisey Esaulkov <toelisey@yandex.ru>
2023-06-08 15:51:48 +0500

    feat: add AboutMe

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import './Techs.css';
 import {
-  TECHS_PROJECT_TITLE,
-  TECHS_PROJECT_PH1,
-  TECHS_PROJECT_PH2,
-  TECHS_PROJECT_LIST,
+  TECHS_TITLE,
+  TECHS_PH1,
+  TECHS_PH2,
+  TECHS_LIST,
 } from './constants'
 import ArticlePage from '../ArticlePage/ArticlePage';
 
 function Techs() {
 
   return(
-    <ArticlePage articlePageMod='techs' articlePageTitleMod='techs__title' articleName={TECHS_PROJECT_TITLE}>
+    <ArticlePage articlePageMod='techs' articlePageTitleMod='techs__title' articleName={TECHS_TITLE}>
       <div className='techs__back' />
-      <p className='techs__ph1'>{TECHS_PROJECT_PH1}</p>
-      <p className='techs__ph2'>{TECHS_PROJECT_PH2}</p>
+      <p className='techs__ph1'>{TECHS_PH1}</p>
+      <p className='techs__ph2'>{TECHS_PH2}</p>
       <ul className='techs__collection'>
-      {TECHS_PROJECT_LIST.map((item, index) => (
+      {TECHS_LIST.map((item, index) => (
         <li
           className='techs__collection-item'
           key={index}
         >{item}</li>
         )
       )}
-        <li></li>
       </ul>
     </ArticlePage>
   );
 }
+
 export default Techs;
--- src/components/Techs/constants.js
+++ src/components/Techs/constants.js
@@ -1,13 +1,11 @@
-const TECHS_PROJECT_TITLE = 'Технологии';
-const TECHS_PROJECT_PH1 = '7 технологий';
-const TECHS_PROJECT_PH2 = 'На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.';
-const TECHS_PROJECT_LIST = [ 'HTML', 'CSS', 'JS', 'React', 'Git', 'Express.js', 'mongoDB'];
-
-
+const TECHS_TITLE = 'Технологии';
+const TECHS_PH1 = '7 технологий';
+const TECHS_PH2 = 'На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.';
+const TECHS_LIST = [ 'HTML', 'CSS', 'JS', 'React', 'Git', 'Express.js', 'mongoDB'];
 
 export {
-  TECHS_PROJECT_TITLE,
-  TECHS_PROJECT_PH1,
-  TECHS_PROJECT_PH2,
-  TECHS_PROJECT_LIST,
+  TECHS_TITLE,
+  TECHS_PH1,
+  TECHS_PH2,
+  TECHS_LIST,
 };
